2c4413f84073ad7c5681b07bc8ccacbbcc6d904e
bohdan.bulazhenko <bohdanbulazhenko@gmail.com>
2020-10-31 22:23:34 +0200

    refactor

---
+++ src/test/java/io/ctdev/tests/JuiceShopNegativeLoginTest.java
@@ -0,0 +1,60 @@
+package io.ctdev.tests;
+
+import com.arakelian.faker.service.RandomData;
+import io.ctdev.BaseTest;
+import io.ctdev.TestProperties;
+import io.ctdev.actions.AccountActions;
+import io.ctdev.entities.SecurityQuestion;
+import io.ctdev.entities.User;
+import io.ctdev.pages.LoginPage;
+import net.bytebuddy.utility.RandomString;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class JuiceShopNegativeLoginTest extends BaseTest {
+    private LoginPage loginPage = new LoginPage();
+
+    private String email;
+    private String password;
+    private String securityQuestionAnswer;
+    private User testUser;
+
+    @BeforeMethod
+    public void beforeTest() {
+        email = RandomString.make(7) + "@jsshop.com";
+        password = RandomString.make(5) + RandomData.get().nextInt(1000, 9999);
+        securityQuestionAnswer = RandomString.make();
+        testUser = new User(email, password, SecurityQuestion.randomQuestion(), securityQuestionAnswer);
+
+        openUrl(TestProperties.config.juiceShopUrl());
+        AccountActions.openLoginform(testUser);
+    }
+
+    @Test
+    public void loginWithInvalidPassword() {
+        loginPage.fillInEmailField(email);
+        loginPage.fillInPasswordField("12");
+        loginPage.clickLoginButton();
+        Assert.assertTrue(loginPage.isInvalidPasswordOrEmailErrorPresent());
+    }
+
+    @Test
+    public void loginWithEmptyEmailAndPassword() {
+        loginPage.fillInEmailField("");
+        loginPage.fillInPasswordField("");
+        loginPage.clickLoginButton();
+        Assert.assertFalse(loginPage.isInvalidPasswordOrEmailErrorPresent());
+
+        loginPage.clickLoginButton();
+        Assert.assertFalse(loginPage.isPleaseProvidePasswordErrorPresent());
+    }
+
+    @Test
+    public void loginWithEmptyEmailField() {
+        loginPage.fillInEmailField("");
+        loginPage.fillInPasswordField(password);
+        loginPage.clickLoginButton();
+        Assert.assertFalse(loginPage.isInvalidPasswordOrEmailErrorPresent());
+    }
+}
